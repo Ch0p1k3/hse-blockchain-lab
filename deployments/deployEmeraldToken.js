1fb36241cc8f3b674734665517311e000c233625
ch0p1k3 <ch00p.228@gmail.com>
2023-02-11 14:49:08 +0300

    fix typo

--- deployments/deployEmeraldToken.js
+++ deployments/deployEmeraldToken.js
@@ -4,7 +4,7 @@ const main = async () => {
     const [deployer] = await ethers.getSigners();
     console.log(`Address deploying the contract --> ${deployer.address}`);
 
-    const tokenFactory = await ethers.getContractFactory("Token");
+    const tokenFactory = await ethers.getContractFactory("EmeraldToken");
     const contract = await tokenFactory.deploy(100000);
 
     console.log(`Token Contract Address --> ${contract.address}`);
